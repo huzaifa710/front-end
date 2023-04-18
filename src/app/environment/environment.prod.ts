6f6c17a6d356056855afb44d3640d36116d1919d
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2023-04-18 17:39:06 +0530

    PROD

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,11 +1,9 @@
-// environment.ts
-
-// export const environment = {
-//     production: true,
-//     apiUrl: 'http://backend-api.ecommerce-project.click:3000/products', // Replace with the URL of your backend API
-//   };
-  
-  export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/products', // Replace with the URL of your backend API
+export const environment = {
+    production: true,
+    apiUrl: 'http://backend-api.ecommerce-project.click:3000/products', // Replace with the URL of your backend API
   };
+  
+  // export const environment = {
+  //   production: false,
+  //   apiUrl: 'http://localhost:3000/products', // Replace with the URL of your backend API
+  // };

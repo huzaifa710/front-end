0727c8d18ccf9ec60371047960b5e8300a2df057
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2023-04-18 16:39:55 +0530

    temp changes

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../product.model';
 import { environment } from '../environment/environment.prod';
@@ -8,52 +8,61 @@ import { environment } from '../environment/environment.prod';
   providedIn: 'root',
 })
 export class ProductService {
-  
   private apiUrl = environment.apiUrl; // replace with your backend API endpoint
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    let header_node = {
-      headers: new HttpHeaders(
-          { 'rejectUnauthorized': 'false' })
-      };
-    return this.http.get<Product[]>(this.apiUrl,header_node);
+    const headers = {
+      headers: new HttpHeaders({
+        rejectUnauthorized: 'false',
+        origin: 'https://main--tubular-dragon-cecba9.netlify.app',
+      }),
+    };
+    return this.http.get<Product[]>(this.apiUrl,headers);
   }
 
   getProductById(id: number): Observable<Product> {
     let header_node = {
-      headers: new HttpHeaders(
-          { 'rejectUnauthorized': 'false' })
-      };
+      headers: new HttpHeaders({
+        rejectUnauthorized: 'false',
+        origin: 'https://main--tubular-dragon-cecba9.netlify.app',
+      }),
+    };
     const url = `${this.apiUrl}/${id}`; // Update the API endpoint URL for getting product by ID
-    return this.http.get<Product>(url,header_node);
+    return this.http.get<Product>(url, header_node);
   }
 
   addProduct(product: Product): Observable<Product> {
     let header_node = {
-      headers: new HttpHeaders(
-          { 'rejectUnauthorized': 'false' })
-      };
-    return this.http.post<Product>(this.apiUrl, product,header_node);
+      headers: new HttpHeaders({
+        rejectUnauthorized: 'false',
+        origin: 'https://main--tubular-dragon-cecba9.netlify.app',
+      }),
+    };
+    return this.http.post<Product>(this.apiUrl, product, header_node);
   }
 
   updateProduct(id: number, product: Product): Observable<Product> {
     let header_node = {
-      headers: new HttpHeaders(
-          { 'rejectUnauthorized': 'false' })
-      };
+      headers: new HttpHeaders({
+        rejectUnauthorized: 'false',
+        origin: 'https://main--tubular-dragon-cecba9.netlify.app',
+      }),
+    };
     const url = `${this.apiUrl}/${id}`; // Update the API endpoint URL for updating product by ID
-    console.log('In the update service',url,product)
-    return this.http.put<Product>(url, product,header_node);
+    console.log('In the update service', url, product);
+    return this.http.put<Product>(url, product, header_node);
   }
 
   deleteProduct(id: number): Observable<void> {
     let header_node = {
-      headers: new HttpHeaders(
-          { 'rejectUnauthorized': 'false' })
-      };
+      headers: new HttpHeaders({
+        rejectUnauthorized: 'false',
+        origin: 'https://main--tubular-dragon-cecba9.netlify.app',
+      }),
+    };
     const url = `${this.apiUrl}/${id}`; // Update the API endpoint URL for deleting product by ID
-    return this.http.delete<void>(url,header_node);
+    return this.http.delete<void>(url, header_node);
   }
 }

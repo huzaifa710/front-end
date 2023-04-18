f92f8398c02db59b9d68939ee0de889c22735961
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2023-04-18 16:09:03 +0530

    http fix

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -8,36 +8,52 @@ import { environment } from '../environment/environment.prod';
   providedIn: 'root',
 })
 export class ProductService {
+  
   private apiUrl = environment.apiUrl; // replace with your backend API endpoint
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    let header_node = {
+      headers: new HttpHeaders(
+          { 'rejectUnauthorized': 'false' })
+      };
+    return this.http.get<Product[]>(this.apiUrl,header_node);
   }
 
   getProductById(id: number): Observable<Product> {
+    let header_node = {
+      headers: new HttpHeaders(
+          { 'rejectUnauthorized': 'false' })
+      };
     const url = `${this.apiUrl}/${id}`; // Update the API endpoint URL for getting product by ID
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url,header_node);
   }
 
   addProduct(product: Product): Observable<Product> {
-    let post_message = product;
     let header_node = {
-        headers: new HttpHeaders(
-            { 'rejectUnauthorized': 'false' })
-        };
-    return this.http.post<Product>(this.apiUrl, product);
+      headers: new HttpHeaders(
+          { 'rejectUnauthorized': 'false' })
+      };
+    return this.http.post<Product>(this.apiUrl, product,header_node);
   }
 
   updateProduct(id: number, product: Product): Observable<Product> {
+    let header_node = {
+      headers: new HttpHeaders(
+          { 'rejectUnauthorized': 'false' })
+      };
     const url = `${this.apiUrl}/${id}`; // Update the API endpoint URL for updating product by ID
     console.log('In the update service',url,product)
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product,header_node);
   }
 
   deleteProduct(id: number): Observable<void> {
+    let header_node = {
+      headers: new HttpHeaders(
+          { 'rejectUnauthorized': 'false' })
+      };
     const url = `${this.apiUrl}/${id}`; // Update the API endpoint URL for deleting product by ID
-    return this.http.delete<void>(url);
+    return this.http.delete<void>(url,header_node);
   }
 }
